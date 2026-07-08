59592e7077efce8fa148df7bbdb9cffe19d6ac24
satyamYCDIE <satyam20sarha@gmail.com>
2026-07-08 21:32:54 +0530

    .

--- src/test/java/seleniumwebdriver/UsingXpath.java
+++ src/test/java/seleniumwebdriver/UsingXpath.java
@@ -19,7 +19,20 @@ public class UsingXpath {
 		driver.findElement(By.xpath("//input[@name='newsletter'][@class='form-control']")).sendKeys("Vittu");
 		
 		//Xpath with inner text()
-		driver.findElement(By.xpath("//a[text()='Blog']")).click();
+		//driver.findElement(By.xpath("//a[text()='Blog']")).click();
+		
+		//checking heading and getting the text of the heading.....
+		boolean displaystatus=driver.findElement(By.xpath("//h5[text()='OpenCart']")).isDisplayed();
+		System.out.println(displaystatus);
+		
+		String textvalue=driver.findElement(By.xpath("//h5[text()='OpenCart']")).getText();
+		System.out.println(textvalue);
+		
+		//Xpath using contains......
+		driver.findElement(By.xpath("//input[contains(@class,'form')]")).sendKeys("Ankit");
+		
+		//Xpath using starts-with.....
+		driver.findElement(By.xpath("//input[starts-with(@class,'form')]")).sendKeys("Sonu");
 	}
 
 }
