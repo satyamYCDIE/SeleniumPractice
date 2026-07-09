25d13eb377f9071e53ba301f33d8a10cff773706
satyamYCDIE <satyam20sarha@gmail.com>
2026-07-10 00:31:01 +0530

    .

--- src/test/java/seleniumwebdriver/WebDriverExamples.java
+++ src/test/java/seleniumwebdriver/WebDriverExamples.java
@@ -1,10 +1,27 @@
 package seleniumwebdriver;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WebDriverExamples {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
 
+		WebElement title = driver.findElement(By.xpath("//h1[normalize-space()='Automation Testing Practice']"));
+		System.out.println(title.isDisplayed());
+		
+		boolean checkRadio=driver.findElement(By.xpath("//input[@id='male']")).isEnabled();
+		System.out.println(checkRadio);
+		
+		WebElement male_Rd=driver.findElement(By.xpath("//label[normalize-space()='Male']"));
+		male_Rd.click();
+		System.out.println(male_Rd.isSelected());
 	}
 
 }
