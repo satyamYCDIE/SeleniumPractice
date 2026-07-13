ef09c50062370289e2280439e09cf299e8c1aba4
satyamYCDIE <satyam20sarha@gmail.com>
2026-07-14 00:24:32 +0530

    .

---
+++ src/test/java/PracticeSelenium/hiddenDropDown.java
@@ -0,0 +1,33 @@
+package PracticeSelenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class hiddenDropDown {
+
+	public static void main(String[] args) 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		
+		//Giving credentials to the page to login......
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+		
+		//Clicking on PIM element....
+		driver.findElement(By.xpath("//a[normalize-space()='PIM']")).click();
+		
+		//Clicking on drop down......
+		driver.findElement(By.xpath("//body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/div[6]/div[1]/div[2]/div[1]/div[1]/div[2]/i[1]")).click();
+		
+		//choosing from hidden drop down.....
+		driver.findElement(By.xpath("//span[normalize-space()='Database Administrator']")).click();
+	}
+
+}
