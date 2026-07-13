daacedd0e82a93bca30fed3352d2df574762d3a7
satyamYCDIE <satyam20sarha@gmail.com>
2026-07-13 20:27:48 +0530

    .

--- src/test/java/PracticeSelenium/selectDropDown.java
+++ src/test/java/PracticeSelenium/selectDropDown.java
@@ -1,6 +1,7 @@
 package PracticeSelenium;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class dropDownPrac {
+public class selectDropDown {
 
 	public static void main(String[] args)
 	{
@@ -24,6 +25,16 @@ public class dropDownPrac {
 		//drpCountry.selectByVisibleText("Germany");
 		//drpCountry.selectByValue("australia");
 		drpCountry.selectByIndex(8);
+		
+		//Capture number of the options from the dropdown.....and using for loop to get the title of each country
+		List<WebElement>options=drpCountry.getOptions();
+		System.out.println("The number of dropdowns available"+options.size());
+		
+		for(WebElement option:options)
+		{
+			System.out.println("Country Name:"+option.getText());
+			
+		}
 	}
 
 }
