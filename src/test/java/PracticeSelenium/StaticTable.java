9d6056c393e054b5f7490ef50d80aa586a8cf0b2
satyamYCDIE <satyam20sarha@gmail.com>
2026-07-14 20:15:25 +0530

    .

---
+++ src/test/java/PracticeSelenium/StaticTable.java
@@ -0,0 +1,32 @@
+package PracticeSelenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StaticTable {
+
+	public static void main(String[] args) 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+		
+		//Capturing the ROWS from the table....
+		int rows=driver.findElements(By.xpath("//table[@name='BookTable']//tr")).size();
+		System.out.println("The number of rows present in the table:"+rows);
+		
+		//Capturing the COLUMNS from the table....
+		int cols=driver.findElements(By.xpath("//table[@name='BookTable']//tr//th")).size();
+		System.out.println("The number of columns present in the table:"+cols);
+		
+		//read data from a specific row and column.....row=5 column=1
+		String BookName=driver.findElement(By.xpath("//table[@name='BookTable']//tr[6]//td[1]")).getText();
+		System.out.println("The bookname fetched from the table:"+BookName);
+	}
+
+}
