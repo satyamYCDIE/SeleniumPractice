31f372801297e72d327be1c70536b65a23068391
satyamYCDIE <satyam20sarha@gmail.com>
2026-07-16 12:09:53 +0530

    .

--- src/test/java/PracticeSelenium/DatePicker.java
+++ src/test/java/PracticeSelenium/DatePicker.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DatePicker {
 	
-	static void selectMonthYear(WebDriver driver,String month,String year)
+	static void selectFutureDate(WebDriver driver,String year,String month,String date)
 	{
 		//Selecting month and year....
 				while(true)
@@ -26,11 +26,8 @@ public class DatePicker {
 					}
 				driver.findElement(By.xpath("//span[@class='ui-icon ui-icon-circle-triangle-e']")).click();
 				}
-	}
-
-	static void selectDate(WebDriver driver,String date)
-	{
-		//selecting date......
+	
+    	//selecting date......
 				List<WebElement> dates=driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']//tbody//tr//a"));
 				
 				for(WebElement dt:dates)
@@ -42,6 +39,37 @@ public class DatePicker {
 					}
 				}
 	}
+	
+	static void selectPastDate(WebDriver driver,String year,String month,String date)
+	{
+			//Selecting month and year....
+					while(true)
+					{
+						
+					String currYear=driver.findElement(By.xpath("//span[@class='ui-datepicker-year']")).getText();
+					String currMonth=driver.findElement(By.xpath("//span[@class='ui-datepicker-month']")).getText();
+					//System.out.println(currYear+""+currMonth);
+					
+					if(currYear.equals(year) && currMonth.equals(month))
+						{
+							break;
+						}
+					driver.findElement(By.xpath("//span[@class='ui-icon ui-icon-circle-triangle-w']")).click();
+					}
+		
+	    	//selecting date......
+					List<WebElement> dates=driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']//tbody//tr//a"));
+					
+					for(WebElement dt:dates)
+					{
+						if(dt.getText().equals(date))
+						{
+							dt.click();
+							break;
+						}
+					}
+		}
+	
 	public static void main(String[] args) 
 	{
 		WebDriver driver = new ChromeDriver();
@@ -57,13 +85,12 @@ public class DatePicker {
 //		Pdate.sendKeys("08/26/2026");
 		
 		//Method 2 using date picker.......
-		String year="2027";
-		String month="June";
-		String date="29";
+		String year="2025";
+		String month="October";
+		String date="5";
 		
 		driver.findElement(By.xpath("//input[@id='datepicker']")).click();
-		selectMonthYear(driver,month,year);
-		selectDate(driver,date);
+		//selectFutureDate(driver,year,month,date);
+		selectPastDate(driver,year,month,date);
 	}
-
 }
