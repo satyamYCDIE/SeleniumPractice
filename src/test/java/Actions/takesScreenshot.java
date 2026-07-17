9713ff716ced7704795d9fc02ca1cf32eaa426b6
satyamYCDIE <satyam20sarha@gmail.com>
2026-07-17 20:34:08 +0530

    .

--- src/test/java/Actions/takesScreenshot.java
+++ src/test/java/Actions/takesScreenshot.java
@@ -4,10 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import org.apache.commons.io.FileUtils;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class takesScreenshot {
@@ -16,14 +17,23 @@ public class takesScreenshot {
 	{
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://www.jqueryscript.net/demo/dual-handles-ticks-cui-slider/");
+		driver.get("https://www.jquery-az.com/7-customized-bootstrap-dropdown-demos-in-menus-navbar-and-tabs/");
 		driver.manage().window().maximize();
 
-		TakesScreenshot ts= (TakesScreenshot)driver;
+		//full screenshot.....
+		/*TakesScreenshot ts= (TakesScreenshot)driver;
 		File sourcefile=ts.getScreenshotAs(OutputType.FILE);
 		File targetfile=new File(System.getProperty("user.dir")+"/screenshots/fullpage.png");
 		//sourcefile.renameTo(targetfile);  //copying source file to target file
+		FileUtils.copyFile(sourcefile, targetfile);*/
+		
+		//specific element screenshot......
+		WebElement specificEle=driver.findElement(By.xpath("//div[@class='footer-widgets-container grid-container']"));
+		File sourcefile=specificEle.getScreenshotAs(OutputType.FILE);
+		File targetfile=new File(System.getProperty("user.dir")+"/screenshots/specificEle.png");
 		FileUtils.copyFile(sourcefile, targetfile);
+		
+		driver.quit();
 	}
 
 }
